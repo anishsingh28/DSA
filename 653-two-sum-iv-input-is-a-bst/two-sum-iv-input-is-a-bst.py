93f697037189cb589796afba3dc4d2474c5cb321
Anish singh <anishsing2005@gmail.com>
2025-12-09 23:25:14 +0530

    Time: 11 ms (23.53%) | Memory: 20.1 MB (13.61%) - LeetSync

---
+++ 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.py
@@ -0,0 +1,17 @@
+
+class Solution:
+    def findTarget(self, root: Optional[TreeNode], k: int) -> bool:
+        seen = set()
+
+        def dfs(node):
+            if not node:
+                return False
+            if k - node.val in seen:
+                return True
+            seen.add(node.val)
+            return dfs(node.right) or dfs(node.left)
+
+        return dfs(root)
+
+
+        
